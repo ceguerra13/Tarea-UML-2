ec89f064098ebe9c3b154068a509c974b87fab4c
ceguerra13 <cegg99@gmail.com>
2023-03-31 21:59:22 -0600

    Se agrega el proyecto

---
+++ template/header.php
@@ -0,0 +1,80 @@
+<!-- url a mi web -->
+<?php $url_base = "http://localhost/MiWebApp/" ?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+    <!-- Estilos css para hacer responsiva las paginas -->
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        th,
+        td {
+            padding: 8px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+
+        tr:hover {
+            background-color: #f5f5f5;
+        }
+
+        /* Estilos para pantallas pequeñas */
+        @media screen and (max-width: 600px) {
+            table {
+                width: 100%;
+            }
+
+            thead {
+                display: none;
+            }
+
+            tr:nth-child(even) {
+                background-color: #f2f2f2;
+            }
+
+            td:before {
+                content: attr(data-th);
+                font-weight: bold;
+                display: inline-block;
+                width: 100px;
+            }
+        }
+    </style>
+</head>
+
+<body>
+    <header>
+        <!-- place navbar here -->
+    </header>
+    <nav class="navbar navbar-expand navbar-light bg-light">
+        <ul class="nav navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link active" href="<?php echo $url_base; ?>" aria-current="page">Sistema <span class="visually-hidden">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo $url_base; ?>modulos/empleados/">Empleados</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo $url_base; ?>modulos/puestos/">Puesto de Empleados</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo $url_base; ?>modulos/usuarios/">Usuarios</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo $url_base; ?>cerrar.php">Cerrar sesión</a>
+            </li>
+        </ul>
+    </nav>
+    <main class="container">
